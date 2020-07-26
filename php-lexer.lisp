a7571f7d7c8c6b7ecb3398ea186ac7df35925835
minghu6 <a19678zy@163.com>
2020-07-26 21:21:01 +0800

    update

--- php-lexer.lisp
+++ php-lexer.lisp
@@ -1,5 +1,3 @@
-
-
 (load "~/.sbclrc")
 
 (ql:quickload "trivia")
@@ -31,7 +29,7 @@
 
 ;;; Some temporary utils
 (defun back (serapeum-queue)
-  (last-elt (qlist serapeum-queue)))
+  (lastcar (qlist serapeum-queue)))
 
 
 
@@ -57,7 +55,7 @@
 
 
 (defmacro gen-char-type-fun-1 (nested-list)
-  "upper case symbol maybe specific for sbcl?"
+  "for (name value) of nested-list => *,name* => value and aaap"
   (eval
    `(cons 'progn
           (flatten-1 (map 'list (lambda (item)
@@ -90,17 +88,23 @@
 
 
 (eval-when (:compile-toplevel :execute :load-toplevel)
-  (defun anycharp (c) t)
+  (defun anycharp (c)
+    (declare (ignore c))
+    t)
 
   ; use serapeum' blankp for sequence and whitespacep for character
   ;; (defun blankp (c)
   ;;   (of c '(#\Space #\Tab #\Return #\Newline)))
+
 
   (defun identityp (c)
     (of c (pp *ascii-letters* *digits* '(#\_))))
 
   (defun phpp (c)
     (of c '(#\p #\h)))
+
+
+
 
   (gen-char-type-fun-1
    '(("slash" #\/)
@@ -115,26 +119,111 @@
     "anychar-but-question")))
 
 
+(defun load-lex-conf ()
+  (read (open "/mnt/d/Coding/CL/smart-code-modifier/php.lex.conf.lisp")))
+
+
+(defvar *lex-conf* (load-lex-conf))
+
+
+(defparameter *lex-dfa-map* (acdr :lex-dfa-map *lex-conf*))
+
+
+(defmacro gen-extra-type-fun (fun-sym char-scope)
+    `(defun ,fun-sym (c)
+       (of c ,(cons 'list char-scope))))
+
+
+(eval-always
+  (defun extra-type-fun-list ()
+    (flet ((parse-char-scope (char-scope)
+             (if (characterp char-scope) `(,char-scope)
+                 (reduce 'pp
+                         (map 'list (lambda (char-placeholder)
+                                      (if (characterp char-placeholder)
+                                          `(,char-placeholder)
+                                          char-placeholder))
+                              char-scope)))))
+      (map 'list (lambda (form)
+                   (let ((type-fun (first form))
+                         (char-scope (second form)))
+                     `(,type-fun ,(parse-char-scope char-scope))
+                     ))
+           (acdr :extra-type-fun *lex-conf*))))
+
+
+  (defmacro gen-extra-type-fun-l ()
+    `(lis-apply gen-extra-type-fun ,(extra-type-fun-list)))
+
+
+  (gen-extra-type-fun-l)
+  )
+
+
+
+(defun raw-type-fun-list ()
+  (remove-duplicates
+   (flatten-1
+    (loop for state-output-map in *lex-dfa-map* collect
+         (loop for each-output in (cdr state-output-map) collect
+              (car each-output))))))
+
+
+
+(raw-type-fun-list)
+
+(mapcar (lambda (x) (~> x cdr first)) '((1 a) (2 b) (3 c)))
+
+(~> '(1 a) cdr first)
+
+;(unless (fboundp type-fun))
 
 ;;; code generation end
 
 
-(defparameter *lex-dfa-map* '((blank-state . ((slash . (comment-start-state t))
-                                              (whitespace . (blank-state nil))
-                                              (<     . (tag-start-state t))
-                                              (identity . (identity-name-state t))
-                                              ))
-                              (tag-start-state . ((question . (tag-start-naming-state nil))
-                                                  
-                                                  ))
-                              (tag-start-naming-state . ((php . (php-tagname-state nil))))
-                              (php-tagname-state . ((php . (php-tagname-state nil))
-                                                    (whitespace . (blank-state t))))
+;;; error define
 
-                              (identity-name-state . ((identity . (identity-name-state nil))
-                                                      (whitespace . (blank-state t))))
+(define-condition unknown-lex-pattern-error (error)
+  ((token
+    :initarg :token
+    :reader token)
+   (c
+    :initarg :c
+    :reader c)
+   (state
+    :initarg :state
+    :reader state)
+   (error-info
+    :initarg :error-info
+    :reader error-info)))
 
-                              ))
+
+(defun format-unknown-lex-pattern-error-info (token c state &key (output t))
+  (format output "~a~%~a: \"~a\" " (show-token token :output nil)
+          state c))
+
+
+(lis-apply gen-restart-fun (("use-blank-state")))
+
+;;; In essence, it just converts the regex pattern into dfa map manually.
+;;; I'm a little genius maybe, [doge]
+;; (defparameter *lex-dfa-map* '((blank-state . ((slash . (comment-start-state t))
+;;                                               (whitespace . (blank-state nil))
+;;                                               (<     . (tag-start-state t))
+;;                                               (identity . (identity-name-state t))
+;;                                               ))
+;;                               (tag-start-state . ((question . (tag-start-naming-state nil))
+;;                                                   ))
+;;                               (tag-start-naming-state . ((php . (php-tagname-state nil))))
+;;                               (php-tagname-state . ((php . (php-tagname-state nil))
+;;                                                     (whitespace . (blank-state t))))
+
+;;                               (identity-name-state . ((identity . (identity-name-state nil))
+;;                                                       (whitespace . (blank-state t))))
+
+;;                               ))
+
+
 
 ;;; Class Lexer
 
@@ -223,9 +312,23 @@
 (defmethod next-step ((automaton lexer) &key (output t))
   (with-slots (source source-ptr states-stack token token-queue lineno colno) automaton
     (if (ptr-nil-p automaton) nil
-        (let ((c (elt source source-ptr)))
+        (let* ((c (elt source source-ptr))
+               (cur-state (first states-stack))
+               (state-multiple-value
+                (unless* (next-state-map cur-state c)
+                         (let ((error-info
+                                (format-unknown-lex-pattern-error-info token c cur-state :output nil)))
+                           (format *ERROR-OUTPUT* error-info)
+                           (restart-case (error 'unknown-lex-pattern-error
+                                                :token token :c c :state cur-state
+                                                :error-info error-info)
+                             (use-value (value) value)
+                             (use-blank-state () '(blank-state t)))
+                           ))))
+
+
           (multiple-value-bind (next-state token-end-p)
-              (values-list (next-state-map (first states-stack) c))
+              (values-list state-multiple-value)
             ;; state-stack <- state
             (push next-state states-stack)
 
@@ -263,7 +366,7 @@
         (setf token nil)))))
 
 
-(defmethod next-token ((automaton lexer))
+(defmethod next-token ((automaton lexer) &key (output t))
   (block nil
     (let ((step-result
            (do ((step-result (next-step automaton :output nil) (next-step automaton :output nil)))
@@ -271,7 +374,7 @@
 
       (flet ((tail-call (automaton)
                (let ((last-token (back (token-queue automaton))))
-                 (show-token last-token)
+                 (show-token last-token :output output)
                  `(,last-token ,(first (states-stack automaton)))
                  )))
         (if (first step-result) (tail-call automaton)
@@ -279,27 +382,37 @@
                 (return)))))))
 
 
-(defun test-lexer ()
+(defmethod run-all ((automaton lexer))
+  (loop while (next-token automaton :output t)))
+
+
+
+(defvar *php-code-slice-1-1*   "<?php
+declare(strict_types=1);" )
+
+
+
+(defun test-lexer (source)
   (format t "~%~%~%")
-  (let ((tested-lexer (make-instance 'lexer :source  "<?php aaa ")))
+  (let ((tested-lexer (make-instance 'lexer :source source)))
     (loop while (next-step tested-lexer :output t))
     (recycle-tail-token tested-lexer)
     (print (qlist (token-queue tested-lexer)))
     ))
 
 
-(test-lexer)
+;(test-lexer  *php-code-slice-1-1*)
 
 
-(defparameter *tested-lexer* (make-instance 'lexer :source  "<?php aaa "))
+
+
+(defparameter *tested-lexer* (make-instance 'lexer :source "<?php aaa "))
+
+(run-all *tested-lexer*)
 
 (next-step *tested-lexer*)
 
 (next-token *tested-lexer*)
-
-(print (token-queue *tested-lexer*))
-
-(next-state-map 'blank-state #\<)
 
 
 (defvar *tested-php-code-slice-1*
